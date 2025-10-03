e71337d8af697f5af75f12c000e50e5f431e3c78
D4viMartinsLima <martinslimafkc@gmail.com>
2025-10-03 15:43:25 -0300

    Game logic implemented

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import turtle
 import pandas
 
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 screen.setup(width=750, height=500)
@@ -8,5 +9,24 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+#In order to work with the data from the column it makes everything easier to turn it into a list
+all_states = data.state.to_list()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+guessed_states = []
+
+while len(guessed_states) < len(all_states) - 1:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another state's name?").title()
+    #Found this method online called title() which converts the guess to Title case
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        #Select only one Item
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(state_data.state.item())
+
+
+screen.exitonclick()
