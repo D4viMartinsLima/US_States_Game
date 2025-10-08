19d98a3e28435e336c9a5362284fc6370701c44a
D4viMartinsLima <martinslimafkc@gmail.com>
2025-10-08 14:01:53 -0300

    Código refatorado utilizando List Comprehension

--- main.py
+++ main.py
@@ -18,14 +18,17 @@ guessed_states = []
 while len(guessed_states) < len(all_states) - 1:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another state's name?").title()
     #Found this method online called title() which converts the guess to Title case
+
     if answer_state == "Exit":
-        missing_states =[]
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+        # missing_states =[]
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
+
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
